a46385e62d5070518d8061542f6403605b4db731
Pablo Piliavsky <pablopiliavsky21@gmail.com>
2023-12-18 18:32:26 -0300

    uhhhhh! first component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PhotosComponent } from './components/photos/photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AlbumsComponent,
+    NavbarComponent,
+    PhotosComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
