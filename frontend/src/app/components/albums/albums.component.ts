8ea479e234c90f4033ed4617e313c4959a6c3608
Pablo Piliavsky <pablopiliavsky21@gmail.com>
2023-12-19 10:47:05 -0300

    general comments for the components

--- frontend/src/app/components/albums/albums.component.ts
+++ frontend/src/app/components/albums/albums.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-albums',
-  templateUrl: './albums.component.html',
-  styleUrls: ['./albums.component.scss']
+  selector: 'app-albums',//forma de referenciar el componente en otros componentes
+  templateUrl: './albums.component.html',//la referencia del template usado para el componente album
+  styleUrls: ['./albums.component.scss'] //referencia del style scss
 })
 export class AlbumsComponent {
 
