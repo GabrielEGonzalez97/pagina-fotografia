264adadeea27f5daf10835f4f5da945a2a799138
Pablo Piliavsky <pablopiliavsky21@gmail.com>
2023-12-27 12:49:02 -0300

    we finish the functionality of menu responsive, but we will need to finish the style of that

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  
+  public active : boolean = true;
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  setActiveShow() : void {
+    this.active = !this.active;
+  }
 
 }
