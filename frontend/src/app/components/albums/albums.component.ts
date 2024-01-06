4e5270846888df870606d565fba4c4ae72ad4d94
Gabriel Gonzalez <gabriel.gonzalez@ibm.com>
2024-01-06 17:52:47 -0300

    Working the filtering togheter with the pagination for the albums

--- frontend/src/app/components/albums/albums.component.ts
+++ frontend/src/app/components/albums/albums.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 import { firstValueFrom } from 'rxjs';
 import { NUMBER_ALBUMS_PER_PAGE } from 'src/app/common/constants';
-import { IPhoto } from 'src/app/common/interfaces';
+import { IAlbum, IPhoto } from 'src/app/common/interfaces';
 import { AlbumService } from 'src/app/services/albums.service';
 import { HttpService } from 'src/app/services/http.service';
 import { IGoogleDriveFields } from 'src/app/services/interfaces';
+import { NavBarService } from 'src/app/services/navbar.service';
 import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
@@ -14,30 +15,37 @@ import { UtilsService } from 'src/app/services/utils.service';
   styleUrls: ['./albums.component.scss'], //referencia del style scss
 })
 export class AlbumsComponent implements OnInit {
-  public firstPhotoByAlbum: Map<string, string> = new Map<string, string>(); // We need this for the moment when the user need to view a one specific
   public photos: IPhoto[] = [];
-  public areImagesLoading: boolean = true;
-  public imagesLoading: boolean[] = [];
+  public photosToShow: IPhoto[] = [];
+
+  public arePhotosLoading: boolean = true;
+  public photosLoading: boolean[] = [];
 
   public pageSize: number = NUMBER_ALBUMS_PER_PAGE;
   public pageIndex: number = 0;
 
   private rootFolderInfo: IGoogleDriveFields[] = [];
   private albumsInfo: IGoogleDriveFields[] = [];
+  private nameOfTheAlbumToSearch: string = '';
 
   constructor(
     private albumService: AlbumService,
     private httpService: HttpService,
+    private navBarService: NavBarService,
     private utilsService: UtilsService
   ) {}
 
   public async ngOnInit(): Promise<void> {
+    this.navBarService.filterEmitted$.subscribe((filter: string) => {
+      this.nameOfTheAlbumToSearch = filter;
+      this.getPaginatedPhotos();
+    });
+
     for (let i: number = 0; i < this.pageSize; i++) {
-      this.imagesLoading.push(true);
+      this.photosLoading.push(true);
     }
 
     let photosCount: number = 0;
-    let photosAux: IPhoto[] = [];
     await firstValueFrom(this.httpService.getRootFolder()).then(
       // First we call the endpoint that returns the info of the root folder in Google Drive
       async (rootFolderInfo: IGoogleDriveFields[]) => {
@@ -49,39 +57,31 @@ export class AlbumsComponent implements OnInit {
           this.albumsInfo.forEach(async (albumInfo: IGoogleDriveFields) => {
             await firstValueFrom(
               this.httpService.getPhotosWithinAlbum(albumInfo.id) // Third we call the endpoint that returns the photos within each album folder
-            ).then(async (photos: IGoogleDriveFields[]) => {
-              if (photos[0]) {
-                const firstPhotoId: string = photos[0].id;
-                await firstValueFrom(
-                  this.httpService.getPhotoById(firstPhotoId) // Fourth we call the endpoint that returns the photo itself
-                ).then((photo: ArrayBuffer) => {
-                  const blob: Blob = new Blob([photo]);
-                  const photoUrl: string = window.URL.createObjectURL(blob);
-                  this.firstPhotoByAlbum.set(albumInfo.id, photoUrl);
-                  const newPhoto: IPhoto = {
-                    photoName: photos[0].name,
-                    photoUrl,
-                    photoCreatedTime: photos[0].createdTime,
-                    album: {
-                      albumId: albumInfo.id,
-                      albumName: albumInfo.name,
-                      albumCreatedTime: albumInfo.createdTime,
-                      photos: photos,
-                    },
-                  };
-                  this.binaryInsertion(photosAux, newPhoto);
-                  photosCount += 1;
-                  if (photosCount === this.albumsInfo.length) {
-                    this.areImagesLoading = false;
-                    this.photos = photosAux;
-                    this.albumService.emitChange(this.photos);
-                  }
-                });
+            ).then(async (photosWithinAlbum: IGoogleDriveFields[]) => {
+              if (photosWithinAlbum[0]) {
+                const album: IAlbum = {
+                  albumId: albumInfo.id,
+                  albumName: albumInfo.name,
+                  albumCreatedTime: albumInfo.createdTime,
+                  photos: photosWithinAlbum,
+                };
+                await this.utilsService
+                  .getPhoto(photosWithinAlbum[0], album)
+                  .then((photo: IPhoto) => {
+                    this.photos.push(photo);
+                    this.photosLoading[this.photos.length - 1] = false;
+                    this.photosLoading.push(true);
+                    photosCount += 1;
+
+                    if (photosCount === this.albumsInfo.length) {
+                      this.arePhotosLoading = false;
+                      this.albumService.emitChange(this.photos);
+                    }
+                  });
               } else {
                 photosCount += 1;
                 if (photosCount === this.albumsInfo.length) {
-                  this.areImagesLoading = false;
-                  this.photos = photosAux;
+                  this.arePhotosLoading = false;
                   this.albumService.emitChange(this.photos);
                 }
               }
@@ -90,45 +90,6 @@ export class AlbumsComponent implements OnInit {
         });
       }
     );
-  }
-
-  private binaryInsertion(photos: IPhoto[], photo: IPhoto): void {
-    if (photos.length === 0) {
-      photos.push(photo);
-      this.imagesLoading[0] = false;
-      this.imagesLoading.push(true);
-    } else {
-      this.binaryHelper(photos, photo, 0, photos.length - 1);
-    }
-  }
-
-  private binaryHelper(
-    photos: IPhoto[],
-    photo: IPhoto,
-    lBound: number,
-    uBound: number
-  ): void {
-    if (uBound <= lBound) {
-      if (
-        photo.album.albumCreatedTime < photos[lBound].album.albumCreatedTime
-      ) {
-        photos.splice(lBound, 0, photo);
-        this.imagesLoading.splice(lBound, 0, false);
-      } else {
-        photos.splice(lBound + 1, 0, photo);
-        this.imagesLoading.splice(lBound + 1, 0, false);
-      }
-    } else {
-      const midPoint: number = Math.floor((uBound - lBound) / 2) + lBound;
-
-      if (
-        photo.album.albumCreatedTime < photos[midPoint].album.albumCreatedTime
-      ) {
-        this.binaryHelper(photos, photo, lBound, midPoint);
-      } else {
-        this.binaryHelper(photos, photo, midPoint + 1, uBound);
-      }
-    }
   }
 
   public navigateTo(route: string): void {
@@ -141,11 +102,22 @@ export class AlbumsComponent implements OnInit {
 
   public getPaginatedLoadingPhotos(): boolean[] {
     const startIndex: number = this.pageIndex * this.pageSize;
-    return this.imagesLoading.slice(startIndex, startIndex + this.pageSize);
+    return this.photosLoading.slice(startIndex, startIndex + this.pageSize);
   }
 
   public getPaginatedPhotos(): IPhoto[] {
     const startIndex: number = this.pageIndex * this.pageSize;
-    return this.photos.slice(startIndex, startIndex + this.pageSize);
+    if (this.nameOfTheAlbumToSearch) {
+      this.photosToShow = this.photos.filter((photo: IPhoto) => {
+        return photo.album.albumName
+          .toLowerCase()
+          .includes(this.nameOfTheAlbumToSearch.toLowerCase());
+      });
+      this.pageIndex = 0;
+    } else {
+      this.photosToShow = this.photos;
+    }
+
+    return this.photosToShow.slice(startIndex, startIndex + this.pageSize);
   }
 }
